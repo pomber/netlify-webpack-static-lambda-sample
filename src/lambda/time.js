b2308b5ed36b90ef79ebab22e222e760e1fbf090
pomber <pombopombopombo@gmail.com>
2018-06-08 18:54:19 -0300

    Use env var

--- src/lambda/time.js
+++ src/lambda/time.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 exports.handler = function(event, context, callback) {
   console.log(event);
   const { code } = event.queryStringParameters;
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ result: `Hello ${code}` })
+    body: JSON.stringify({ result: `${process.env.HELLO} ${code}` })
   });
 };
