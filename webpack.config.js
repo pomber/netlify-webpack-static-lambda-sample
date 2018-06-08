79b561757a15bce4bcce0b025ec53d33dcdcbb85
pomber <pombopombopombo@gmail.com>
2018-06-08 15:42:59 -0300

    Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin("dist", {}),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new WebpackMd5Hash()
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    overlay: true,
+    proxy: {
+      "/api": "http://localhost:9999"
+    }
+  },
+  node: {
+    fs: "empty",
+    module: "empty"
+  }
+};
